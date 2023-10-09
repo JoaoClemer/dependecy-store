f162302e5a1fdc03e55364d80ff0bc2232f435ed
João Clemer <joaoclemerlima@gmail.com>
2023-10-09 19:52:24 -0300

    Customer repository

---
+++ Repositories/Contract/ICustomerRepository.cs
@@ -0,0 +1,9 @@
+﻿using DependencyStore.Models;
+
+namespace DependencyStore.Repositories.Contract
+{
+    public interface ICustomerRepository
+    {
+        Task<Customer?> GetByIdAsync(string customerId);
+    }
+}
