ed96e936ccc563233e17cade214964a7e82b1432
João Clemer <joaoclemerlima@gmail.com>
2023-10-10 08:40:15 -0300

    Resolving dependencies

--- Controllers/OrderController.cs
+++ Controllers/OrderController.cs
@@ -1,10 +1,7 @@
-﻿using Dapper;
-using DependencyStore.Models;
+﻿using DependencyStore.Models;
 using DependencyStore.Repositories.Contract;
 using DependencyStore.Services.Contracts;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Data.SqlClient;
-using RestSharp;
 
 namespace DependencyStore.Controllers;
 
@@ -29,9 +26,9 @@ public class OrderController : ControllerBase
     public async Task<IActionResult> Place(string customerId, string zipCode, string promoCode, int[] products)
     {
         var customer = await _customerRepository.GetByIdAsync(customerId);
-       
-        if (customer == null) 
-        { 
+
+        if (customer == null)
+        {
             return NotFound();
         }
 
@@ -44,7 +41,7 @@ public class OrderController : ControllerBase
         var order = new Order(deliveryFee, discount, new List<Product>());
 
         return Ok($"Order {order.Code} generated successfully!");
-        
-        
+
+
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,4 +1,20 @@
+using DependencyStore;
+using DependencyStore.Extensions;
+using DependencyStore.Repositories;
+using DependencyStore.Repositories.Contract;
+using DependencyStore.Services;
+using DependencyStore.Services.Contracts;
+using Microsoft.Data.SqlClient;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSingleton<Configuration>();
+builder.Services.AddSqlConnection("CONN_STR");
+builder.Services.AddExternalServices();
+builder.Services.AddRepositories();
+
+
+
 builder.Services.AddControllers();
 
 var app = builder.Build();
